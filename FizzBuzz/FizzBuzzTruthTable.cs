78db0bb5851a6df5205c8b9cdb33614501b34550
YuriyNskiy <ruanaruy@gmail.com>
2018-10-30 00:22:42 +0200

    Initial commit

---
+++ FizzBuzz/FizzBuzzTruthTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzTruthTable
+    {
+        private static readonly Dictionary<(bool, bool), string> _fizzBuzzTruthTable
+            = new Dictionary<(bool, bool), string>
+            {
+                [(false, false)] = null,
+                [(false, true)] = "Buzz",
+                [(true, false)] = "Fizz",
+                [(true, true)] = "FizzBuzz"
+            };
+
+        public static void Run()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                var fizzBuzz = _fizzBuzzTruthTable[(i % 3 == 0, i % 5 == 0)] ?? i.ToString();
+
+                Console.WriteLine(fizzBuzz);
+            }
+        }
+    }
+}
